8ac79da2b1ce351787b468a69485daba85261d1d
Matias Mariani <hi@matiasmariani.io>
2017-02-06 20:09:43 -0500

    hello world

---
+++ app/scripts/projects.js
@@ -0,0 +1,37 @@
+var myProjects = {
+    projects: [{
+        title: 'Mockup',
+        description: 'A functional webpage replicated from an image file.',
+        readme: 'https://github.com/matiasironyard/1.4-pixel-perfect/blob/master/README.md',
+        link: 'https://matiasironyard.github.io/1.4-pixel-perfect',
+        img: './images/pixelperfect.png'
+    }, {
+        title: 'Github Profile',
+        description: 'A functional replica of my Github page.',
+        readme: 'https://github.com/matiasironyard/4.4-Github-Profile/blob/master/README.md',
+        link: 'https://matiasironyard.github.io/4.4-Github-Profile',
+        img: './images/github.png'
+    }, {
+        title: 'Image Board',
+        description: 'A Backbone image board app that lets users create a picture blog.',
+        readme: 'https://github.com/matiasironyard/7.2-Image-Board-2/blob/master/README.md',
+        link: 'https://matiasironyard.github.io/7.2-Image-Board-2',
+        img: './images/imageboard.png'
+    }, {
+        title: 'Menu & Order',
+        description: 'A React restaurant menu that displays an order from selected items.',
+        readme: 'https://github.com/matiasironyard/7.4-Majestic-Thai/blob/master/README.md',
+        link: 'https://matiasironyard.github.io/7.4-Majestic-Thai',
+        img: './images/restaurant.png'
+    }, {
+        title: 'In The Mood',
+        description: 'A downtown Greenville restaurant search engine featuring a restaurant dashboard for business owners and a favorites page for users, powered by the Yelp Fusion API.',
+        readme: 'https://github.com/matiasironyard/Final-Project-Main-Street/blob/master/README.md',
+        link: 'https://matiasironyard.github.io/Final-Project-Main-Street',
+        img: './images/inthemood.png'
+    }]
+};
+
+module.exports = {
+    myProjects: myProjects
+};
