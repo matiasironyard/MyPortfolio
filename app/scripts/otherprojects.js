9315e76dfd2b08cc50f188ec89414dbabaea4189
Matias Mariani <hi@matiasmariani.io>
2017-08-06 00:26:20 -0400

    fix deploy

--- app/scripts/otherprojects.js
+++
@@ -1,7 +0,0 @@
-{
-    title: 'Image Board',
-    description: 'A Backbone image board app that lets users create a picture blog.',
-    readme: 'https://github.com/matiasironyard/7.2-Image-Board-2/blob/master/README.md',
-    link: 'https://matiasironyard.github.io/7.2-Image-Board-2',
-    img: './images/imageboard.png'
-}, 
--- app/scripts/projects.js
+++ app/scripts/projects.js
@@ -1,42 +1,43 @@
 var myProjects = {
-    projects: [{
-        title: 'Mockup',
-        description: 'A functional webpage replicated from an image file.',
-        readme: 'https://github.com/matiasironyard/1.4-pixel-perfect/blob/master/README.md',
-        link: 'https://matiasironyard.github.io/1.4-pixel-perfect',
-        img: './images/pixelperfect.png'
+  projects: [
+    {
+      title: 'Mockup',
+      description: 'A functional webpage replicated from an image file.',
+      readme: 'https://github.com/matiasironyard/1.4-pixel-perfect/blob/master/README.md',
+      demoLink: 'https://matiasironyard.github.io/1.4-pixel-perfect',
+      img: './images/pixelperfect.png'
     }, {
-        title: 'Github Profile',
-        description: 'A functional replica of my Github page.',
-        readme: 'https://github.com/matiasironyard/4.4-Github-Profile/blob/master/README.md',
-        link: 'https://matiasironyard.github.io/4.4-Github-Profile',
-        img: './images/github.png'
+      title: 'Github Profile',
+      description: 'A functional replica of my Github page.',
+      readme: 'https://github.com/matiasironyard/4.4-Github-Profile/blob/master/README.md',
+      demoLink: 'https://matiasironyard.github.io/4.4-Github-Profile',
+      img: './images/github.png'
     }, {
-        title: 'Menu & Order',
-        description: 'A React restaurant menu that displays an order from selected items.',
-        readme: 'https://github.com/matiasironyard/7.4-Majestic-Thai/blob/master/README.md',
-        link: 'https://matiasironyard.github.io/7.4-Majestic-Thai',
-        img: './images/restaurant.png'
+      title: 'Menu & Order',
+      description: 'A React restaurant menu that displays an order from selected items.',
+      readme: 'https://github.com/matiasironyard/7.4-Majestic-Thai/blob/master/README.md',
+      demoLink: 'https://matiasironyard.github.io/7.4-Majestic-Thai',
+      img: './images/restaurant.png'
     }, {
       title: 'Behind the Red Door',
-      description:  "A sample website for Karen Moore's Book, Behind The Red Door, published by the Advantage Media Group.",
+      description: 'A sample website for Karen Moore\'s Book, Behind The Red Door, published by the Advantage Media Group.',
       readme: 'https://github.com/matiasironyard/Advantage-Sample/blob/master/README.md',
-      link: 'https://matiasironyard.github.io/Advantage-Sample/',
+      demoLink: 'https://matiasironyard.github.io/Advantage-Sample/',
       img: './images/AdvantageSample.jpg'
     }, {
-        title: 'In The Mood',
-        description: 'A downtown Greenville restaurant search engine featuring a restaurant dashboard for business owners and a favorites page for users, powered by the Yelp Fusion API.',
-        readme: 'https://github.com/matiasironyard/Final-Project-Main-Street/blob/master/README.md',
-        link: 'https://matiasironyard.github.io/Final-Project-Main-Street',
-        img: './images/inthemood.png'
+      title: 'In The Mood',
+      description: 'A downtown Greenville restaurant search engine featuring a restaurant dashboard for business owners and a favorites page for users, powered by the Yelp Fusion API.',
+      readme: 'https://github.com/matiasironyard/Final-Project-Main-Street/blob/master/README.md',
+      demoLink: 'https://matiasironyard.github.io/Final-Project-Main-Street',
+      img: './images/inthemood.png'
     }, {
-        title: 'React Redux SWAPI',
-        description: 'A React Redux powered SWAPI explorer .',
-        readme: 'https://github.com/matiasironyard/react-redux-starwars/blob/master/README.md',
-        link: 'https://matiasironyard.github.io/react-redux-starwars/#/',
-        img: './images/swapi.png'
+      title: 'React Redux SWAPI',
+      description: 'A React Redux powered SWAPI explorer .',
+      readme: 'https://github.com/matiasironyard/react-redux-starwars/blob/master/README.md',
+      demoLink: 'https://matiasironyard.github.io/react-redux-starwars/#/',
+      img: './images/swapi.png'
     }]
 };
 module.exports = {
-    myProjects: myProjects
+  myProjects: myProjects
 };
