dfa6b6b4551b098f5ee48016c13f96a776135c25
Matias Mariani <hi@matiasmariani.io>
2017-08-29 20:30:46 -0400

    skills handlebars

--- app/scripts/index.js
+++ app/scripts/index.js
@@ -4,6 +4,14 @@ var Handlebars = require('Handlebars');
 $(document).ready(function() {
   $('.parallax').parallax();
   $('.button-collapse').sideNav();
+  $('.button-collapse-skills').sideNav({
+      menuWidth: 300, // Default is 300
+      menuHeight: 200,
+      edge: 'right', // Choose the horizontal origin
+      closeOnClick: true, // Closes side-nav on <a> clicks, useful for Angular/Meteor
+      draggable: true, // Choose whether you can drag to open on touch screens,
+    }
+  );
   $('.scrollspy').scrollSpy();
   $('.tooltipped').tooltip({delay: 50});
   $('.expand').click(function() {
---
+++ app/scripts/techs.js
@@ -0,0 +1,36 @@
+var techs = {
+  techs: [
+    {
+      img: "./images/html5.png"
+    },
+    {
+      img: "./images/css3.png"
+    },
+    {
+      img: "./images/js.png"
+    },
+    {
+      img: "./images/jquery.png"
+    },
+    {
+      img: "./images/Boostrap_logo.svg.png"
+    },
+    {
+      img: "./images/backbone.png"
+    },
+    {
+      img: "./images/react-logo.png"
+    },
+    {
+      img: "./images/redux.png"
+    },
+    {
+      img: "./images/node.png"
+    }
+
+
+   ]
+};
+module.exports = {
+  techs: thech
+};
